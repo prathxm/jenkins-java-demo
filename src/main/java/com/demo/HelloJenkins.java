2172740032e54096a0ae3c7080bb8f192ad77d3c
Aditi Thakur <13aditithakur@gmail.com>
2025-11-03 03:14:00 +0530

    Update HelloJenkins.java

--- src/main/java/com/demo/HelloJenkins.java
+++ src/main/java/com/demo/HelloJenkins.java
@@ -5,7 +5,7 @@ public class HelloJenkins {
         return "Hello from Jenkins Pipeline!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(greet());
+     public static void main(String[] args) {
+        System.out.println(new HelloJenkins().getMessage());
     }
 }
